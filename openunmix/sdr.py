005fab31761fea3357829edaa988450eb803439a
ayush055 <ayushagarwal2500@gmail.com>
2022-07-11 00:45:07 -0400

    Fixing sdr.py

--- openunmix/sdr.py
+++ openunmix/sdr.py
@@ -5,7 +5,7 @@ def get_scores(model_name):
     method = museval.MethodStore()
     method.load('models/' + model_name + '.pandas')
     method.agg_frames_tracks_scores()
-    return method.scores
+    return
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="MUSDB18 Evaluation", add_help=False)
