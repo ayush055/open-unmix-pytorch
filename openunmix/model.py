d63cfe99e3c570ce9e162b74569c5a9708a6759e
ayu055 <ayushagarwal2500@gmail.com>
2022-07-13 00:30:39 -0400

    fixing reformer

--- openunmix/model.py
+++ openunmix/model.py
@@ -50,8 +50,8 @@ class OpenUnmix(nn.Module):
         else:
             self.nb_bins = self.nb_output_bins
 
-        print(f"nb_bins: {self.nb_bins}")
-        print(f"nb_output_bins: {self.nb_output_bins}")
+        # print(f"nb_bins: {self.nb_bins}")
+        # print(f"nb_output_bins: {self.nb_output_bins}")
 
         # self.hidden_size = hidden_size
         
@@ -191,7 +191,7 @@ class OpenUnmix(nn.Module):
         # print("Y shape before fc layer:", y.size())
 
         x = x.reshape(nb_samples, nb_frames, nb_channels * self.nb_bins)
-        y = y.reshape(y_samples, y_frames, y_channels * self.nb_bins)
+        y = y.reshape(y_samples, y_frames, y_channels * self.nb_output_bins)
 
         enc_keys = self.encoder(x)
         x = self.decoder(y, keys=enc_keys)
