6658c043b0537be37524a8dd7c933552d8210b52
ayu055 <ayushagarwal2500@gmail.com>
2022-07-13 21:29:32 -0400

    debugging validatoin

--- openunmix/model.py
+++ openunmix/model.py
@@ -267,6 +267,7 @@ class OpenUnmix(nn.Module):
 
         x = x[1:-1, :, :]
         transformer_out = transformer_out[1:, :, :]
+        print(transformer_out)
         x = torch.cat([x, transformer_out], -1)
 
         # first dense stage + batch norm
