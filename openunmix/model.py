5453a2cdbfaca4cc0e87bbec9921e69fc3f127b0
neha-peddinti <neha.peddinti@gmail.com>
2022-07-15 13:34:53 -0400

    debugging

--- openunmix/model.py
+++ openunmix/model.py
@@ -249,6 +249,9 @@ class OpenUnmix(nn.Module):
         # x = torch.cat([x, transformer_out], -1)
 
         # apply cnn
+
+        x = torch.reshape(x, (nb_samples, 1, self.hidden_size * 2, nb_frames))
+        x = x.expand(nb_samples, 3, self.hidden_size * 2, nb_frames)
         x = self.vgg16(x)
         print('shape of x after vgg: ', x.shape)
 
