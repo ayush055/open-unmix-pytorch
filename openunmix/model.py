4636b6b6746e7ff19c1502ac3c8e5fe83faeeecd
ayu055 <ayushagarwal2500@gmail.com>
2022-07-15 04:43:46 -0400

    updating repository

--- openunmix/model.py
+++ openunmix/model.py
@@ -55,8 +55,8 @@ class OpenUnmix(nn.Module):
 
         self.bn1 = BatchNorm1d(hidden_size)
 
-        self.pos_encoder1 = PositionalEncoding(self.nb_bins, dropout=0.5)
-        encoder_layer1 = nn.TransformerEncoderLayer(d_model=self.nb_bins, nhead=4, dropout=0.5, activation='gelu')
+        self.pos_encoder1 = PositionalEncoding(self.nb_bins * nb_channels, dropout=0.5)
+        encoder_layer1 = nn.TransformerEncoderLayer(d_model=self.nb_bins * nb_channels, nhead=4, dropout=0.5, activation='gelu')
         self.encoder1 = TransformerEncoder(
             encoder_layer1, num_layers=3
         )
@@ -67,7 +67,7 @@ class OpenUnmix(nn.Module):
             lstm_hidden_size = hidden_size // 2
 
         self.lstm = LSTM(
-            input_size=hidden_size,
+            input_size=self.nb_bins * nb_channels,
             hidden_size=lstm_hidden_size,
             num_layers=nb_layers,
             bidirectional=not unidirectional,
