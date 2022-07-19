ccf9ed9d7af6f4cae909b061c50630166a3a89a0
ayu055 <ayushagarwal2500@gmail.com>
2022-07-19 14:36:33 -0400

    trying transformer with window

--- train.py
+++ train.py
@@ -62,10 +62,11 @@ def valid(args, unmix, encoder, device, valid_sampler):
                 if i + img_width > num_frames:
                     padding = (0, i + img_width - num_frames)
                     X_tmp, Y_tmp = F.pad(X_tmp, padding, mode='constant', value=0), F.pad(Y_tmp, padding, mode='constant', value=0)
-                    print(X_tmp, Y_tmp)
-                    print("Indexing from {} to {}".format(i, i+img_width))
-                    print(X_tmp.shape, Y_tmp.shape, X.size(-1), img_width, hop_length)
                     break
+                print(X_tmp, Y_tmp)
+                print("Indexing from {} to {}".format(i, i+img_width))
+                print(X_tmp.shape, Y_tmp.shape, X.size(-1), img_width, hop_length)
+                    
                 # Y_hat = unmix(X_tmp, Y_tmp)
                 # loss += torch.nn.functional.mse_loss(Y_hat, Y)
             loss /= i
