3c2446febb84e96338badcad348e78074c0b6f66
ayu055 <ayushagarwal2500@gmail.com>
2022-07-19 14:55:58 -0400

    trying transformer with window

--- train.py
+++ train.py
@@ -59,7 +59,7 @@ def valid(args, unmix, encoder, device, valid_sampler):
             num_frames = X.size(-1)
             preds = []
             for i in range(0, num_frames, hop_length):                    
-                print("Indexing from {} to {}".format(i, i+hop_length))
+                print("Indexing from {} to {}".format(i, i+img_width))
                 X_tmp, Y_tmp = X[:, :, :, i:(i + img_width)], Y[:, :, :, i:(i + img_width)]
                 if i + img_width > num_frames:
                     padding = (0, i + img_width - num_frames)
